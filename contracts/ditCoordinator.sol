0f8e8cfa3ad83e413e4f420b9c2aff88968a8aea
marvinkruse <5999310+marvinkruse@users.noreply.github.com>
2019-02-27 12:12:25 +0100

    Change repository to bytes32 and add vote settings
    Changed the repository specified to a bytes32 from a string. This will
    be a keccak256 hash of the git provider and the repo name (e.g.
    github.com/user/reponame).
    Also added some more vote settings to influence to min and max
    vote durations directly at deploy-time.

--- contracts/ditCoordinator.sol
+++ contracts/ditCoordinator.sol
@@ -17,7 +17,7 @@ contract ditCoordinator {
     using SafeMath for uint256;
     
     struct ditRepository {
-        string repository;
+        bytes32 repository;
         address ditContract;
         uint256 votingMajority;
         uint256 mintingMethod;
@@ -30,7 +30,7 @@ contract ditCoordinator {
     KNWVotingContract KNWVote;
     KNWTokenContract KNWToken;
 
-    mapping (string => ditRepository) repositories;
+    mapping (bytes32 => ditRepository) repositories;
 
     constructor(address _KNWTokenAddress, address _KNWVotingAddress) public {
         require(_KNWVotingAddress != address(0) && _KNWTokenAddress != address(0), "KNWVoting and KNWToken address can't be empty");
@@ -42,20 +42,34 @@ contract ditCoordinator {
 
     /**
      * @dev Creats a new ditCraft-based repository
-     * @param _repository The name of the repository
+     * @param _repository The descriptor of the repository (e.g. keccak256("github.com/example_repo"))
      * @param _label1 The first knowledge label of this repository (see KNWToken)
      * @param _label2 The second knowledge label of this repository (see KNWToken)
      * @param _label3 The third knowledge label of this repository (see KNWToken)
-     * @param _voteSettings A uint256 array (with a length of three) containing the necessary 
-     * voting majority, the minting and the burning method
+     * @param _voteSettings A uint256 array (with a length of seven) containing the vote settings: 
+     *  [0] = votingMajority in percent (50-100)
+     *  [1] = mintingMethod (0 or 1)
+     *  [2] = burningMethod (0 or 1)
+     *  [3] = minVoteCommitDuration (in seconds)
+     *  [4] = maxVoteCommitDuration (in seconds)
+     *  [5] = minVoteOpenDuration (in seconds)
+     *  [6] = maxVoteOpenDuration (in seconds)
      * @return The address of the newly deployed ditContract
      */
-    function initRepository(string _repository, string _label1, string _label2, string _label3, uint256[3] _voteSettings) external returns (address newDitContract) {
-        require(bytes(_repository).length > 0, "Name of the repository can't be empty");
+    function initRepository(bytes32 _repository, string _label1, string _label2, string _label3, uint256[7] _voteSettings) external returns (address newDitContract) {
+        require(_repository != 0, "Repository descriptor can't be zero");
         require(repositories[_repository].ditContract == address(0), "Repository can't already have a ditContract");
         
         // Deploying a new ditContract according to the provided settings
-        newDitContract = address(new ditContract(KNWVotingAddress, address(this), _repository, _label1, _label2, _label3));
+        newDitContract = address(new ditContract(
+            KNWVotingAddress,
+            address(this),
+            _repository,
+            _label1,
+            _label2, 
+            _label3,
+            [_voteSettings[3], _voteSettings[4], _voteSettings[5], _voteSettings[6]]
+            ));
 
         // Storing the new dit-based repository
         repositories[_repository] = ditRepository({
@@ -74,11 +88,10 @@ contract ditCoordinator {
 
     /**
      * @dev Gets a ditCraft-based repositories ditContract address
-     * @param _repository The name of the repository
+     * @param _repository The descriptor of the repository (e.g. keccak256("github.com/example_repo"))
      * @return The address of the deployed ditContract
      */
-    function getRepository(string _repository) external view returns (address repositoryAddress) {
-        require(bytes(_repository).length > 0, "Name of the repository can't be empty");
+    function getRepository(bytes32 _repository) external view returns (address repositoryAddress) {
         return repositories[_repository].ditContract;
     }
 }
