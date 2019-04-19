0d66c358d6764bea324ae8ccc8d086b17573696c
marvinkruse <marvinkruse@users.noreply.github.com>
2019-04-19 11:55:03 +0200

    Update live migration according to changes

--- migrations/2_main_migration.js
+++ migrations/2_main_migration.js
@@ -8,15 +8,13 @@ module.exports = async function(deployer) {
   await deployer.link(SafeMath, KNWToken);
   let tokenInstance = await deployer.deploy(KNWToken);
   await deployer.link(SafeMath, KNWVoting);
-  let votingInstance = await deployer.deploy(KNWVoting);
+  let votingInstance = await deployer.deploy(KNWVoting, KNWToken.address, "0x0000000000000000000000000000000000000000");
   await deployer.link(SafeMath, ditCoordinator);
-  await deployer.deploy(ditCoordinator, KNWToken.address, KNWVoting.address);
-  await tokenInstance.setVotingAddress(KNWVoting.address);
+  await deployer.deploy(ditCoordinator, KNWToken.address, KNWVoting.address, "0x0000000000000000000000000000000000000000");
+  await tokenInstance.addVotingContract(KNWVoting.address);
   console.log("\n   Post-Deployment Calls")
   console.log("   ----------------------");
-  console.log("   > Post-Deployment Call 1/3 done");
-  await votingInstance.setCoordinatorAddress(ditCoordinator.address);
-  console.log("   > Post-Deployment Call 2/3 done");
-  await votingInstance.setTokenAddress(KNWToken.address);
-  console.log("   > Post-Deployment Call 3/3 done\n");
+  console.log("   > Post-Deployment Call 1/2 done");
+  await votingInstance.addDitCoordinatorAddress(ditCoordinator.address);
+  console.log("   > Post-Deployment Call 2/2 done\n");
 };
